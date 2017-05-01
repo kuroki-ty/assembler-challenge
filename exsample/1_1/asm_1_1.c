14c446abfc5dc9aa24b209aa68b2680745050019
tomotaka-yamasaki <tomotaka.yamasaki@aktsk.jp>
2017-05-01 17:02:37 +0900

    課題 1-1を追加

---
+++ exsample/1_1/asm_1_1.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+
+int sum(int* array, int size)
+{
+  int ret;
+  int tmp;
+  int i = 0;
+
+/************************************
+*  for(int i = 0; i < size; i++) {
+*    ret += array[i];
+*  }
+************************************/
+  asm volatile (
+    "loop: \t\n"
+      "mov (%[array], %[i], 4), %[tmp] \t\n"  // array[i]の値をtmpに格納  arrayの先頭ptr+(i*4byte)の値 → tmp
+      "add %[tmp], %[ret] \t\n"     // ret += tmp
+      "add $0x01, %[i] \t\n"        // i++
+      "cmp %[i], %[size] \t\n"      // i < size の結果をステータスレジスタに格納
+      "ja loop \t\n"                     // i < size がtrueならloopラベルにジャンプ
+    :[ret]"=&r"(ret), [tmp]"+&r"(tmp), [i]"+&r"(i)
+    :[array]"r"(array), [size]"r"(size)
+  );
+
+  return ret;
+}
+
+int main(void)
+{
+  int array[5] = {1, 2, 3, 4, 5};
+  int size = 5;
+  int val = sum(array, size);
+
+  printf("%d\n", val);
+
+  return 0;
+}
