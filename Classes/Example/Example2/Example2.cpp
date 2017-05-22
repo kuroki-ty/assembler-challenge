ebc73963447cc16cc487262b6b3c2b785209c4ff
tomotaka-yamasaki <tomotaka.yamasaki@aktsk.jp>
2017-05-23 00:35:04 +0900

    アライメントを意識してloadするbyte数を判断する

--- Classes/Example/Example2/Example2.cpp
+++ Classes/Example/Example2/Example2.cpp
@@ -15,8 +15,15 @@ std::string Example2::calcAnswer()
     char buf1[10];
     size_t size = 5;
 
+    // 確認用
+    int b1 = (unsigned)buf1 % 4;
+    int b2 = (unsigned)buf2 % 4;
+    printf("buf1は %d\n", b1);
+    printf("buf2は %d\n", b2);
+
     int c = 0;
     copyMemory(buf1, buf2, size, &c);
+    printf("%d\n", c);
 
     _answer = buf1;
 
@@ -34,9 +41,9 @@ void Example2::copyMemory(void *dst, const void *src, size_t size, int *c)
                   "B Loop \t\n"
 
                   "Loop1:"
-                    "LDR r1, [%[c]] \t\n"
-                    "ADD r1, r1, #1 \t\n"
-                    "STR r1, [%[c]] \t\n"
+                    "LDR r2, [%[c]] \t\n"
+                    "ADD r2, r2, #1 \t\n"
+                    "STR r2, [%[c]] \t\n"
 
                     "LDRB r0, [%[src]], #1 \t\n"
                     "STRB r0, [%[dst]], #1 \t\n"
@@ -44,9 +51,9 @@ void Example2::copyMemory(void *dst, const void *src, size_t size, int *c)
                     "B Loop \t\n"
 
                   "Loop2:"
-                    "LDR r1, [%[c]] \t\n"
-                    "ADD r1, r1, #20 \t\n"
-                    "STR r1, [%[c]] \t\n"
+                    "LDR r2, [%[c]] \t\n"
+                    "ADD r2, r2, #20 \t\n"
+                    "STR r2, [%[c]] \t\n"
 
                     "LDRH r0, [%[src]], #2 \t\n"
                     "STRH r0, [%[dst]], #2 \t\n"
@@ -54,9 +61,9 @@ void Example2::copyMemory(void *dst, const void *src, size_t size, int *c)
                     "B Loop \t\n"
 
                   "Loop4:"
-                    "LDR r1, [%[c]] \t\n"
-                    "ADD r1, r1, #40 \t\n"
-                    "STR r1, [%[c]] \t\n"
+                    "LDR r2, [%[c]] \t\n"
+                    "ADD r2, r2, #400 \t\n"
+                    "STR r2, [%[c]] \t\n"
 
                     "LDR r0, [%[src]], #4 \t\n"
                     "STR r0, [%[dst]], #4 \t\n"
@@ -64,14 +71,25 @@ void Example2::copyMemory(void *dst, const void *src, size_t size, int *c)
                     "B Loop \t\n"
 
                   "Loop:"
+                  "Label_load4:"
                     "CMP %[size], #4 \t\n"
-                    "BGE Loop4 \t\n"
+                    "BLT Label_load2 \t\n"
+
+                    "ANDS r1, %[src], 0x3 \t\n"
+                    "BEQ Loop4 \t\n"
+
+                  "Label_load2:"
                     "CMP %[size], #2 \t\n"
-                    "BGE Loop2 \t\n"
+                    "BLT Label_load1 \t\n"
+
+                    "ANDS r1, %[src], 0x1 \t\n"
+                    "BEQ Loop2 \t\n"
+
+                  "Label_load1:"
                     "CMP %[size], #1 \t\n"
                     "BGE Loop1 \t\n"
                   :[dst]"+r"(dst), [src]"+r"(src), [size]"+r"(size)
                   :[c]"r"(c)
-                  :"r0", "r1"
+                  :"r0", "r1", "r2"
                   );
 }
