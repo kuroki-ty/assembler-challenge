7771a65c7064baf7c10c926e16ff9c289f56fae5
kuroki-ty <t.yamasaki11@gmail.com>
2017-05-30 09:32:02 +0900

    フィルタ側を第2オペランドでシフトさせることで2命令削除

--- Classes/Example/Example3/Example3.cpp
+++ Classes/Example/Example3/Example3.cpp
@@ -40,26 +40,24 @@ uint32_t* Example3::swapByteAsm(uint32_t* src)
     asm volatile (
                   "LDR r4, [%[src]] \t\n"
                   "MOV r6, #0 \t\n"
+                  "MOV r7, 0x000000ff \t\n"
 
-                  "AND r5, r4, 0xff \t\n"
+                  "AND r5, r4, r7 \t\n"
                   "ORR r6, r6, r5, LSL #24 \t\n"
-                  "LSR r4, r4, #8 \t\n"
 
-                  "AND r5, r4, 0xff \t\n"
-                  "ORR r6, r6, r5, LSL #16 \t\n"
-                  "LSR r4, r4, #8 \t\n"
-
-                  "AND r5, r4, 0xff \t\n"
+                  "AND r5, r4, r7, LSL #8 \t\n"
                   "ORR r6, r6, r5, LSL #8 \t\n"
-                  "LSR r4, r4, #8 \t\n"
 
-                  "AND r5, r4, 0xff \t\n"
-                  "ORR r6, r6, r5 \t\n"
+                  "AND r5, r4, r7, LSL #16 \t\n"
+                  "ORR r6, r6, r5, LSR #8 \t\n"
+
+                  "AND r5, r4, r7, LSL #24 \t\n"
+                  "ORR r6, r6, r5, LSR #24 \t\n"
 
                   "STR r6, [%[dst]] \t\n"
                   :
                   :[dst]"r"(dst), [src]"r"(src)
-                  :"r4", "r5", "r6"
+                  :"r4", "r5", "r6", "r7"
                   );
 
     return dst;
